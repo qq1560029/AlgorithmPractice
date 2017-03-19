6c96f4b366ce5423281ca99e382902b64f0efdfd
qq1560029 <244699680@qq.com>
2017-03-19 21:11:41 +0800

    add new

--- 01背包 DP.cpp
+++ 01背包 DP.cpp
@@ -15,7 +15,7 @@ int main(void) {
 	}
 	vector<int> table(coin, 0);
 	for (int i = 0; i < n; i++) {
-		for (int j = coin - 1; j >= need[i]; j--)
+		for (int j = coin - 1; j >= need[i]; j--)   //在只选前i种商品时 花j这么多钱能获得的最大收益  因逆序，故每种商品i只有选或不选两种状态
 		{
 			table[j] = max(table[j], table[j - need[i]] + value[i]);
 		}
--- 完全背包 DP.cpp
+++ 完全背包 DP.cpp
@@ -15,7 +15,7 @@ int main(void) {
 	}
 	vector<int> table(coin, 0);
 	for (int i = 0; i < n;i++) {
-		for (int j = need[i]; j<coin; j++)
+		for (int j = need[i]; j<coin; j++)   //在只选前i种商品时 花j这么多钱能获得的最大收益  顺序遍历，故每种商品i可选多次
 		{
 			table[j] = max(table[j], table[j - need[i]] + value[i]);
 		}
